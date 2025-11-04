6e6d4fbd6324f1004d2e5e1c548321c9d1bcda7a
Connor <127959098+C0NNYA@users.noreply.github.com>
2025-11-04 11:03:24 +0000

    Menus fixed
    Set each canvas to be not active when not the focus

--- Assets/Scripts/CameraSwap.cs
+++ Assets/Scripts/CameraSwap.cs
@@ -4,15 +4,22 @@ public class CameraSwap : MonoBehaviour
 {
     public GameObject MainCam;
     public GameObject SettingsCam;
+    public GameObject MainCanvas;
+    public GameObject SettingsCanvas;
 
     public void MainSwitch()
     {
         MainCam.SetActive(false);
         SettingsCam.SetActive(true);
+        MainCanvas.SetActive(false);
+        SettingsCanvas.SetActive(true);
+
     }
     public void SettingSwitch()
     {
         MainCam.SetActive(true);
         SettingsCam.SetActive(false);
+        SettingsCanvas.SetActive(false);
+        MainCanvas.SetActive(true);
     }
 }
