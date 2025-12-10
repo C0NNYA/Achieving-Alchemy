d0b6559c6dbc2877958899078863828c62ff503a
Connor <connorjamesmaider@hotmail.com>
2025-12-10 18:49:59 +0000

    looking
    player looks now

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,11 @@ public class PlayerMove : MonoBehaviour
     public InputActionReference move;
     public InputActionReference jump;
     public float jumpForce;
+    public InputActionReference look;
+    public float lookSensitivity = 2f;
+    public Transform cameraTransform;
+    private float _xRotation = 0f;
+
 
 
     private void Update()
@@ -17,16 +22,29 @@ public class PlayerMove : MonoBehaviour
         _moveDirection = move.action.ReadValue<Vector3>();
     }
 
+    private void LateUpdate()
+    {
+        Vector2 lookInput = look.action.ReadValue<Vector2>();
+        float mouseX = lookInput.x * lookSensitivity;
+        float mouseY = lookInput.y * lookSensitivity;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+
 
     private void FixedUpdate()
     {
         Vector3 currentY = new Vector3(0, rb.linearVelocity.y, 0);
 
-        Vector3 horizontalVelocity = new Vector3(
-            _moveDirection.x * moveSpeed,
-            0,
-            _moveDirection.z * moveSpeed
-        );
+        // Convert input into world-space movement based on player rotation
+        Vector3 moveDir = transform.TransformDirection(new Vector3(_moveDirection.x, 0, _moveDirection.z));
+        Vector3 horizontalVelocity = moveDir * moveSpeed;
+
 
         rb.linearVelocity = horizontalVelocity + currentY;
     }
@@ -34,12 +52,18 @@ public class PlayerMove : MonoBehaviour
 
     private void OnEnable()
     {
-        jump.action.started += Jump; 
+        jump.action.started += Jump;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
     }
 
     private void OnDisable()
     {
         jump.action.started -= Jump;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
     }
 
     private void Jump(InputAction.CallbackContext context)
