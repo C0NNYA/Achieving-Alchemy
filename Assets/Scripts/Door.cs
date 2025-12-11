52c23b17bc578a07f71bcb2ae3f692109b8a06dd
Connor <connorjamesmaider@hotmail.com>
2025-12-11 10:28:12 +0000

    check

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,17 +3,14 @@ using UnityEngine;
 public class Door : MonoBehaviour, IInteractable
 {
     [Header("Scene loader")]
-    public string sceneToLoad;          // Name of the scene to load
-    public string destinationSpawnPoint; // Spawn point name in the new scene
-
+    public string sceneToLoad;
+    public string destinationSpawnPoint;
     public void Interact()
     {
         Debug.Log("INTERACT WITH DOOR: " + gameObject.name);
 
-        // Save where the player should spawn in the next scene
         SceneTransition.spawnPointName = destinationSpawnPoint;
 
-        // Load the new scene
         Object.FindAnyObjectByType<SceneLoader>().LoadSceneByName(sceneToLoad);
     }
 }
